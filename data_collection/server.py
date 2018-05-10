16839ca56ca459180686a02cdf6006edc0a12ec4
Shanna <shanamathew135@gmail.com>
2018-05-09 22:15:52 -0400

    fixed more bugs

--- data_collection/server.py
+++ data_collection/server.py
@@ -126,29 +126,36 @@ except:
         elif action == 6:  # IR_SEND
             ##SHANA DONE##
             add_action(game_ID=-1, player_ID, action)
+            return "-1,0,-1"
+    elif action == 7:  # IR_RECEIVE
+    ##SHANA DONE##
+    c.execute(
+        '''SELECT player_ID FROM gestures_table WHERE (checked = 0 AND action = 6 AND player_ID != ? AND timing > ?);''',
+        (player_ID, ir_receive_threshold))
+    results = c.fetchone()
+    if results is None:  # if no ir_send
+        add_action(game_ID=-1, player_ID, action)
+    else:
+        add_action(game_ID=-1, player_ID, action, checked=1)
+    player_1 = results["player_ID"]
+    c.execute(
+        '''UPDATE gestures_table SET checked = 1 WHERE (player_ID = ? AND action = 6) ORDER BY timing DESC LIMIT 1;''',
+        (player_1,))  # set ir_send checked to 1
+    c.execute('''INSERT into game_table VALUES (null,?,?,?,?,?);''', (player_1, 5, player_ID, 5, 1))  # create new game
+    c.execute('''SELECT game_ID FROM game_table WHERE (player_1 = ? AND player_2 = ? AND game_state = 1);''',
+              (player_1, player_ID))
+    game_ID = c.fetchone()["game_ID"]
+    return str(5) + "," + str(1) + "," + str(game_ID)
 
-        elif action == 7:  # IR_RECEIVE
-            ##SHANA DONE##
-            c.execute(
-                '''SELECT player_ID FROM gestures_table WHERE (checked = 0 AND action = 6 AND player_ID != ? AND timing > ?);''',
-                (player_ID, ir_receive_threshold))
-            results = c.fetchone()
-            if results is None:
-                add_action(game_ID=-1, player_ID, action)
-            else:
-                add_action(game_ID=-1, player_ID, action, checked=1)
-                player_2 = results["player_ID"]
-            c.execute('''INSERT into game_table VALUES (null,?,?,?,?,?);''', (player_ID, 5, player_2, 5, 1))
 
-
-            # send stuff to 2
-        elif method = "GET":
-    ## SHANA DONE ##
+    # send stuff to 2
+elif method = "GET":
+## SHANA DONE ##
 c.execute('''SELECT * FROM game_table WHERE (player_ID = ? AND game_state = 1);''', (player_ID,))
 results = c.fetchone()
 if results is None:
     return "-1,0,-1"
-return str(results[health_1]) + "," + str(results[game_state]) + "," + str(results[game_ID])
+return str(results['health_1']) + "," + str(results['game_state']) + "," + str(results['game_ID'])
 
 
 def add_action(game_ID, player_ID, action_ID, time=now, checked=0):
@@ -169,7 +176,7 @@ def update_game_info(game_ID, player_1, health_1, player_2, health_2, game_state
     """
     c.execute('''UPDATE game_table SET health_1 = ?, health_2 = ?, game_state = ? WHERE game_ID = ?;''',
               (health_1, health_2, game_state, game_ID))
-    return state
+    return game_state
 
 
 def extract_game_info(game_ID):
@@ -194,18 +201,18 @@ def extract_player_gestures(player_1, player_2):
     value in that database row to 1. If they're not, one of the players 
     is None. Returns a dictionary of the player name and their action. 
     """
-    recent_player_1 = c.execute(
-        '''SELECT * FROM gesture_table WHERE player_ID = player_1 ORDER BY timing DESC;''').fetchone()
-    recent_player_2 = c.execute(
-        '''SELECT * FROM gesture_table WHERE player_ID = player_2 ORDER BY timing DESC;''').fetchone()
+    recent_player_1 = c.execute('''SELECT * FROM gesture_table WHERE player_ID = ? ORDER BY timing DESC;''',
+                                (player_1,)).fetchone()
+    recent_player_2 = c.execute('''SELECT * FROM gesture_table WHERE player_ID = ? ORDER BY timing DESC;''',
+                                (player_2,)).fetchone()
 
 
-time_dif = recent_player_1['timing'] - recent_player_2['timing'].total_seconds()
+time_dif = (recent_player_1['timing'] - recent_player_2['timing']).microseconds()
 # times are greater than 1 sec apart and recent_player_1 was first
-if time_dif < -1:
+if time_dif < -1000000:
     recent_player_2 = None
 # times are greater than 1 sec apart and recent_player_2 was first
-elif time_dif > 1:
+elif time_dif > 1000000:
     recent_player_1 = None
 
 if recent_player_1['checked'] != 0:
@@ -214,20 +221,22 @@ if recent_player_2['checked'] != 0:
     recent_player_2 = None
 
     # If row has an action that is not block, change the check value to 1
-    if recent_player_1 != None and recent_player_1['action'] != 4:
-c.execute('''UPDATE gestures_table SET checked = 1 WHERE player_ID = player_1 ORDER BY timing DESC LIMIT 1;''')
-if recent_player_1 != None and recent_player_1['action'] != 4:
-    c.execute('''UPDATE gestures_table SET checked = 1 WHERE player_ID = player_2 ORDER BY timing DESC LIMIT 1;''')
+    if recent_player_1 is not None and recent_player_1['action'] != 4:
+c.execute('''UPDATE gestures_table SET checked = 1 WHERE (player_ID = ? and timing = ?);''',
+          (player_1, recent_player_1['timing']))
+if recent_player_2 is not None and recent_player_2['action'] != 4:
+    c.execute('''UPDATE gestures_table SET checked = 1 WHERE (player_ID = ? and timing = ?);''',
+              (player_2, recent_player_2['timing']))
 
 # Return dictionary of player name and their action
-if player_1 == None and player_2 == None:
-    return {recent_player_1['player_ID']: None, recent_player_2['player_ID']: None}
-elif player_1 == None:
-    return {recent_player_1['player_ID']: None, recent_player_2['player_ID']: recent_player_2['action']}
-elif player_2 == None:
-    return {recent_player_1['player_ID']: recent_player_1['action'], recent_player_2['player_ID']: None}
+if recent_player_1 is None and recent_player_2 is None:
+    return {player_1: None, player_2: None}
+elif recent_player_1 is None:
+    return {player_1: None, player_2: recent_player_2['action']}
+elif recent_player_2 is None:
+    return {player_1: recent_player_1['action'], player_2: None}
     else:
-    {recent_player_1['player_ID']: recent_player_1['action'], recent_player_2['player_ID']: recent_player_2['action']}
+    return {player_1: recent_player_1['action'], player_2: recent_player_2['action']}
 
 
 def check_game_start():
